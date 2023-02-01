9c11a258f34563f7c18b62ce5b27fbcfaaf49c59
John Rooney <rooneyjohn@gmail.com>
2023-01-31 20:40:03 -0500

    commit tree 1121b8442c59a8a865363c713638498ed27f4a0d

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,58 @@
-# your code goes here
+class Person
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(value)
+        if value <= 0
+            @happiness = 0
+        elsif value >= 10
+            @happiness = 10
+        else
+            @happiness = value
+        end
+    end
+
+    def happy?
+        if @happiness > 7
+            true
+        else
+            false
+        end
+    end
+
+    def hygiene=(value)
+        if value <= 0
+            @hygiene = 0
+        elsif value >= 10
+            @hygiene = 10
+        else
+            @hygiene = value
+        end
+    end
+
+    def clean?
+        if @hygiene > 7
+            true
+        else
+            false
+        end
+    end
+    def happy?
+        if @happy > 7
+            true
+        else
+            false
+        end
+    end
+    def get_paid(amount)
+        self.bank_account = self.bank_account + amount
+        "all about the benjamins"
+    end
+end
